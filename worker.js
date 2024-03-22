7c1789364d2c5692ff75f5a6538b89a6c99739a2
Axe <45731066+Axe-l@users.noreply.github.com>
2024-03-22 14:55:27 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -5,8 +5,8 @@ const ADMIN_UID = ENV_ADMIN_UID // your user id, get it from https://t.me/userna
 
 const NOTIFY_INTERVAL = 3600 * 1000;
 const fraudDb = 'https://raw.githubusercontent.com/LloydAsp/nfd/main/data/fraud.db';
-const notificationUrl = 'https://raw.githubusercontent.com/LloydAsp/nfd/main/data/notification.txt'
-const startMsgUrl = 'https://raw.githubusercontent.com/LloydAsp/nfd/main/data/startMessage.md';
+const notificationUrl = 'https://raw.githubusercontent.com/Axe-l/nfd-AIFBI/blob/main/data/notification.txt'
+
 
 const enable_notification = true
 /**
@@ -107,7 +107,7 @@ async function onMessage (message) {
     if(!message?.reply_to_message?.chat){
       return sendMessage({
         chat_id:ADMIN_UID,
-        text:'使用方法，回复转发的消息，并发送回复消息，或者`/block`、`/unblock`、`/checkblock`等指令'
+        text:'这里发送聊天消息'
       })
     }
     if(/^\/block$/.exec(message.text)){
